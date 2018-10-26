004ffab0b1e8defb80dc4f097afa2c1d75ff1ca8
harsha roddam <rharsha1997@gmail.com>
2018-10-26 10:55:44 +0530

    working

--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsComponent } from './details/details.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   declarations: [DetailsComponent]
 })
